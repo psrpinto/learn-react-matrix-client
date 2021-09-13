366099b4a1197efe8b726d1216321407264f5114
Paulo Pinto <paulo.pinto@automattic.com>
2021-09-13 14:41:14 +0100

    Implement room selection

--- src/RoomList.js
+++ src/RoomList.js
@@ -1,12 +1,26 @@
 import React from "react";
 
 class RoomList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedRoomName: ''
+        };
+    }
+
+    setSelected(room) {
+        this.setState({selectedRoomName: room.name})
+    }
+
     render() {
         return (
             <div className="roomList">
                 <ul style={{listStyle: 'none'}}>
                     {this.props.rooms.map(room => (
-                        <li key={room.name}>
+                        <li key={room.name}
+                            className={this.state.selectedRoomName === room.name ? 'selected' : ''}
+                            onClick={() => this.setSelected(room)}>
                             <span>{room.name}</span>
                         </li>
                     ))}
