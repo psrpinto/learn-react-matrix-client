d30f43dfb8c28776fb58666e1c77e7e858f329cc
Paulo Pinto <paulo.pinto@automattic.com>
2021-09-14 17:46:45 +0100

    Convert App to tsx

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,9 +3,23 @@ import React from "react";
 import * as sdk from "matrix-js-sdk";
 import RoomList from "./RoomList";
 import Timeline from "./Timeline";
+import {MatrixClient, Room} from "matrix-js-sdk";
 
-class App extends React.Component {
-    constructor(props) {
+enum Status {
+    Loading = "loading",
+    Loaded = "loaded",
+    Failed = "Failed",
+}
+
+interface State {
+    status: Status;
+    selectedRoom: Room|null;
+}
+
+class App extends React.Component<{}, State> {
+    client: MatrixClient;
+
+    constructor(props: {}) {
         super(props);
         this.client = sdk.createClient({
             baseUrl: 'http://localhost:8008',
@@ -13,24 +27,24 @@ class App extends React.Component {
             accessToken: 'syt_YWRtaW4_WgzGYbdhFRZdOJTGxUzx_0NPo3T',
         });
         this.state = {
-            status: 'loading',
+            status: Status.Loading,
             selectedRoom: null,
         };
     }
 
     render() {
-        if (this.state.status === 'loaded') {
+        if (this.state.status === Status.Loaded) {
             return (
                 <div className="App">
                     <RoomList client={this.client}
-                              onSelectionChange={(newlySelectedRoom) => this.setState({selectedRoom: newlySelectedRoom})}/>
+                              onSelectionChange={(newlySelectedRoom: Room) => this.setState({selectedRoom: newlySelectedRoom})}/>
                     <Timeline client={this.client}
                               room={this.state.selectedRoom}/>
                 </div>
             );
         }
 
-        if (this.state.status === 'loading') {
+        if (this.state.status === Status.Loading) {
             return (
                 <div className="LoadingScreen">
                     Loading....
@@ -46,19 +60,17 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        this.client.once('sync', state => {
-            if (state !== 'PREPARED') {
-                this.setState({status: 'failed'});
+        this.client.once('sync', status => {
+            if (status !== 'PREPARED') {
+                this.setState({status: Status.Failed});
                 return;
             }
 
-            this.setState({
-                status: 'loaded',
-            });
+            this.setState({status: Status.Loaded});
         });
 
         this.client.startClient({}).catch(error => {
-            this.setState({status: 'failed'});
+            this.setState({status: Status.Failed});
             console.log('Initial sync failed: ' + error);
         });
     }
